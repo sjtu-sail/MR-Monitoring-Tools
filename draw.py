88745cc2132982ef3fadf6c01952924f75089929
WangPuyue <wangpy1489@sjtu.edu.cn>
2019-01-16 14:37:01 +0800

    skew draw now

--- draw.py
+++ draw.py
@@ -1,8 +1,8 @@
 # import matplotlib.pypot as plt
 import numpy as np
-# import matplotlib as mpl
-#
-# mpl.use('Agg')
+import matplotlib as mpl
+
+mpl.use('Agg')
 from matplotlib.pyplot import xlabel, ylabel, title, grid, plot, savefig
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
