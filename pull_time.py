7021af282d8a10206c2a55e35399dae38f6df310
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2019-03-15 16:30:36 +0800

    modify script to add error detect

--- pull_time.py
+++ pull_time.py
@@ -12,22 +12,28 @@ import os
 class TimePuller:
     properties = {}
 
-    def __init__(self, app_id):
-        raw_str = self.pull_properties(app_id)
-        self.process_properties(raw_str)
+    def __init__(self):
         pass
 
-    @staticmethod
-    def pull_properties(raw_id):
+    def pull_properties(self, raw_id):
         with os.popen("/home/wuchunghsuan/hadoop-2.8.5/bin/yarn application -status " + raw_id, "r") as f:
-            return f.read()
+            raw_str = f.read()
+        self.properties = self.process_properties(raw_str)
+        if self.properties == {}:
+            return -1
+        else:
+            return 0
 
     def process_properties(self, raw_str):
         s = raw_str.split("\n\t")
+        if len(s) < 3:
+            return {}
+        ret_pro = {}
         for i in s:
             i = i.strip()
             i = i.split(":")
-            self.properties[i[0].strip()] = i[1].strip()
+            ret_pro[i[0].strip()] = i[1].strip()
+        return ret_pro
 
     def get_property(self, name):
         return self.properties[name]
@@ -40,6 +46,7 @@ class TimePuller:
 
 
 if __name__ == '__main__':
-    te = TimePuller("application_1552279621115_0003")
+    te = TimePuller()
+    te.pull_properties("application_1552279621115_0003")
     ans = te.properties
     print(ans)
