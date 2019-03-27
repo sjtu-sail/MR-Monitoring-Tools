459ace38b434c51771670a93ab95efc4c191ead3
root <wangpy1489@sjtu.edu.cn>
2019-03-27 20:36:10 +0800

    fix

--- prometheus/pull_time.py
+++ prometheus/pull_time.py
@@ -1,53 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2019/3/11 16:36
+# @Time    : 2019/3/25 16:45
 # @Author  : laazy
 # @Site    : 
 # @File    : pull_time.py
 # @Software: PyCharm
 
-import os
+import time
+import sys
 
 
 class TimePuller:
-    properties = {}
+    begin_time = 0
+    end_time = sys.maxsize
+    nodes = ["2", "3", "4", "5", "6", "7"]
 
     def __init__(self):
         pass
 
     def pull_properties(self, raw_id):
-        with os.popen("/home/wuchunghsuan/hadoop-2.8.5/bin/yarn application -status " + raw_id, "r") as f:
-            raw_str = f.read()
-        self.properties = self.process_properties(raw_str)
-        if self.properties == {}:
+        for i in self.nodes:
+            with open("/home/wuchunghsuan/MR-Monitoring-Tools/output/" + raw_id + "-" + i, "r") as f:
+                raw_str = f.readlines()
+                self.begin_time = max(self.begin_time,self.process_properties(raw_str[0]))
+                self.end_time = min(self.end_time, self.process_properties(raw_str[-1]))
+        if self.begin_time == 0:
             return -1
         else:
             return 0
 
     @staticmethod
     def process_properties(raw_str):
-        s = raw_str.split("\n\t")
-        if len(s) < 3:
-            return {}
-        ret_pro = {}
-        for i in s:
-            i = i.strip()
-            i = i.split(":")
-            ret_pro[i[0].strip()] = i[1].strip()
-        return ret_pro
-
-    def get_property(self, name):
-        return self.properties[name]
+        if len(raw_str) < 24:
+            return 0
+        raw_str = raw_str[0:23]
+        left_ms = int(raw_str[20:23])
+        timestamp = int(time.mktime(time.strptime(raw_str[0:19], "%Y-%m-%d %H:%M:%S"))) * 1000
+        return left_ms + timestamp
 
     def get_start_time(self):
-        return int(self.get_property("Start-Time"))
+        return self.begin_time
 
     def get_finish_time(self):
-        return int(self.get_property("Finish-Time"))
+        return self.end_time
 
 
 if __name__ == '__main__':
     te = TimePuller()
-    te.pull_properties("application_1552279621115_0003")
-    ans = te.properties
-    print(ans)
+    te.pull_properties("application_1553323617432_0105")
+    print(te.begin_time)
+    print(te.end_time)
